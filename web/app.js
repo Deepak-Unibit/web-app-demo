1c2694b4b5c872f2784395402fddcfa3018a100e
Deepak <deepak.unibit@gmail.com>
2024-10-01 12:00:48 +0530

    cdfgcb

--- web/app.js
+++ web/app.js
@@ -1,6 +1,6 @@
 window.state = {
     hello: 'world'
-    const tg = window.Telegram.WebApp;
+    var tg = window.Telegram.WebApp;
     const initData = tg.initData;
     const user = tg.initDataUnsafe.user;
     console.log(user);
