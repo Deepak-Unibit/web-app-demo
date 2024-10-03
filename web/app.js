6ddf7ce72124ab96f9e77b6c4b17deb666ad2720
Deepak <deepak.unibit@gmail.com>
2024-10-03 19:28:22 +0530

    fgjrtyc

--- web/app.js
+++ web/app.js
@@ -2,9 +2,6 @@
  const initData = tg.initData;
  const user = tg.initDataUnsafe.user;
 
- console.log(urlParams);
- console.log(startParam);
-
 window.state = {
     userData: JSON.stringify(user)
 }
