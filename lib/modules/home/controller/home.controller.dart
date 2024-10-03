7692efa598c8d79c6823a5af121da3bd930c4678
Deepak <deepak.unibit@gmail.com>
2024-10-03 19:58:02 +0530

    fgjrtyc

--- lib/modules/home/controller/home.controller.dart
+++ lib/modules/home/controller/home.controller.dart
@@ -201,7 +201,13 @@ class HomeController extends GetxController with GetTickerProviderStateMixin {
   }
 
   onJoinChannelClick(int type) {
-    String telegramLink = 'https://t.me/test2channelforsaquib';
+    String telegramLink = "";
+    if(type==0) {
+      telegramLink = 'https://t.me/test2channelforsaquib';
+    }
+    else {
+      telegramLink = 'https://t.me/testchannel3forsaquib';
+    }
     // 'https://t.me/share/url?url=https://t.me/catizenbot/gameapp?startapp=rp_1365932&text=%F0%9F%92%B0Catizen%3A%20Unleash%2C%20Play%2C%20Earn%20-%20Where%20Every%20Game%20Leads%20to%20an%20Airdrop%20Adventure!%0A%F0%9F%8E%81Let%27s%20play-to-earn%20airdrop%20right%20now!'
 
     html.window.open(telegramLink, '_blank');
