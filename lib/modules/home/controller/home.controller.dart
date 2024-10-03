25b5aa9855cfe097f8804843cc04a9ac6c1f5054
Deepak <deepak.unibit@gmail.com>
2024-10-03 16:02:21 +0530

    dfsgdfr

--- lib/modules/home/controller/home.controller.dart
+++ lib/modules/home/controller/home.controller.dart
@@ -1,12 +1,9 @@
 import 'dart:convert';
-import 'dart:math' as math;
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:web_app_demo/api/call.api.dart';
 import 'package:web_app_demo/api/url.api.dart';
 import 'package:web_app_demo/components/loadingPage/loadingPage.component.dart';
-import 'package:web_app_demo/helper/lottie.helper.dart';
 import 'package:web_app_demo/models/response.model.dart';
 import 'package:web_app_demo/models/spin.model.dart';
 import 'dart:js' as js;
@@ -154,7 +151,7 @@ class HomeController extends GetxController with GetTickerProviderStateMixin {
   }
 
   onJoinChannelClick() {
-    String telegramLink = 'https://t.me/Wheel24Bot';
+    String telegramLink = 'https://t.me/testchannelforsaquib';
 
     // 'https://t.me/share/url?url=https://t.me/catizenbot/gameapp?startapp=rp_1365932&text=%F0%9F%92%B0Catizen%3A%20Unleash%2C%20Play%2C%20Earn%20-%20Where%20Every%20Game%20Leads%20to%20an%20Airdrop%20Adventure!%0A%F0%9F%8E%81Let%27s%20play-to-earn%20airdrop%20right%20now!'
 
