9f32026d83b90c490835531d660ed5403fecf126
Deepak <deepak.unibit@gmail.com>
2024-10-07 17:55:29 +0530

    dfxbd

--- web/app.js
+++ web/app.js
@@ -1,6 +1,10 @@
  const tg = window.Telegram.WebApp;
  const initData = tg.initData;
  const user = tg.initDataUnsafe.user;
+ console.log(tg);
+ console.log(tg.initData);
+ console.log(tg.initDataUnsafe.user);
+ console.log();
 
 window.state = {
     userData: JSON.stringify(user)
