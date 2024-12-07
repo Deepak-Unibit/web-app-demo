fcd845a648b07e437d55b1e4cd6c712af514f1c7
Deepak <deepak.unibit@gmail.com>
2024-12-07 19:46:01 +0530

    ghjdf

--- lib/api/app.const.dart
+++ lib/api/app.const.dart
@@ -2,15 +2,15 @@ class AppConst {
   static String authToken = '';
 
   // Development
-  // static const String appId = "khdgchjdk_dev";
-  // static const String baseUrl = "https://dev.fun11.in:8005";
-  // static const String baseUrl2 = "https://dev.fun11.in:8006";
-  // static const String encryptionKey = "chgadsc_dev";
+  static const String appId = "khdgchjdk_dev";
+  static const String baseUrl = "https://dev.fun11.in:8005";
+  static const String baseUrl2 = "https://dev.fun11.in:8006";
+  static const String encryptionKey = "chgadsc_dev";
 
   // Production
-  static const String appId = "6544646519841891984_PROD";
-  static const String baseUrl = "https://tgb-2.ludo24.app";
-  static const String baseUrl2 = "https://tgb-1.ludo24.app";
-  static const String encryptionKey = "5648946198491298498_PROD";
+  // static const String appId = "6544646519841891984_PROD";
+  // static const String baseUrl = "https://tgb-2.ludo24.app";
+  // static const String baseUrl2 = "https://tgb-1.ludo24.app";
+  // static const String encryptionKey = "5648946198491298498_PROD";
 
 }
